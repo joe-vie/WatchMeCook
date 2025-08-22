ca7112aabc50b9d5da638aad36877176f6af9809
joe-vie <marcemg101@gmail.com>
2025-08-21 22:28:52 -0400

    fixed player death and added a new background & enemy

---
+++ scripts/slime.gd
@@ -0,0 +1,22 @@
+extends Area2D
+const SPEED = 60
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		body.die()
+
+var direction = 1
+@onready var ray_cast_right: RayCast2D = $RayCastRight
+@onready var ray_cast_left: RayCast2D = $RayCastLeft
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if ray_cast_right.is_colliding():
+		direction = -1
+		animated_sprite.flip_h = true
+	if ray_cast_left.is_colliding():
+		direction = 1
+		animated_sprite.flip_h = false
+	position.x += direction * SPEED * delta
+	# moving 60 pixels per second but it is now moving independent from the frame rate :3
